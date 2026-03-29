d20e56c20c5adfa7804e4bbd7e0792ad38b4fd70
KevinVigne <kev.vigne@gmail.com>
2026-03-29 19:25:41 +0200

    Models Created

--- exercice2/products/models.py
+++ exercice2/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    #Création des propriétés
+    name = models.CharField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    expiration_date = models.DateField()
+
+    #Changement de l'affichage des item  en  (nom - prix € . Date de péremption : date_de_péremption)
+    def __str__(self):
+        return f"{self.name} - {self.price} € . Date de péremption : {self.expiration_date}"
