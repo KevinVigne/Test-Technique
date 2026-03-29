d6c1fcd11fa36ef76d8afedb1ec215c5ee2f1340
KevinVigne <kev.vigne@gmail.com>
2026-03-29 17:28:56 +0200

    Création des Templates products et invoices

--- exercice2/exercice2/settings.py
+++ exercice2/exercice2/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Ajout des app products et invoices
+    'products',
+    'invoices',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'exercice2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],#Ajout du dossier templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
