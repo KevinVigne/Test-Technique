556cb33b228d2eac514ed767c8ad9b896cc265e8
KevinVigne <kev.vigne@gmail.com>
2026-03-31 01:31:57 +0200

    Added a forgotten Path to invoices

--- exercice2/exercice2/urls.py
+++ exercice2/exercice2/urls.py
@@ -21,5 +21,6 @@ from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+    path('invoices/', include('invoices.urls')),
     path('', RedirectView.as_view(url='/products/', permanent=False)),
 ]
