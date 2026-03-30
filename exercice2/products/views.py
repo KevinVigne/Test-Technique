ddce44f10e54105452c95c44afb4812240380173
KevinVigne <kev.vigne@gmail.com>
2026-03-30 02:21:28 +0200

    CRUD on a product done

--- exercice2/products/views.py
+++ exercice2/products/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from django.core.paginator import Paginator
+from .models import Product
 
-# Create your views here.
+# Fonction d'affichage de la liste des Produits
+def product_list(request):
+    products = Product.objects.all()
+    paginator = Paginator(products , 8)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request , 'products/product_list.html' , {'page_obj' : page_obj})
+
+# Fonction de création d'un produit
+def product_create(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        price = request.POST['price']
+        expiration_date = request.POST['expiration_date']
+        Product.objects.create(name = name, price = price, expiration_date = expiration_date)
+        messages.success(request,f'Produit « {name} » créé avec succès.' )
+        return redirect('product_list')
+    return render(request, 'products/product_form.html' , {'title' : 'Nouveau produit'})
+
+# Fonction de modification d'un produit
+def product_edit(request, id):
+    product = get_object_or_404(Product, pk=id)
+    if request.method == 'POST':
+        product.name = request.POST['name']
+        product.price = request.POST['price']
+        product.expiration_date = request.POST['expiration_date']
+        product.save()
+        messages.success(request ,f'Produit « {product.name} »  modifié avec succès.')
+        return redirect('product_list')
+    return render(request, 'products/product_form.html' , {'title' : 'Modifier le produit' , 'product':product} )
+
+# Fonction Supprimer un produit
+def product_delete(request, id):
+    product = get_object_or_404(Product, pk=id)
+    if request.method == 'POST':
+        name = product.name
+        product.delete()
+        messages.success(request , f'Produit « {name} » supprimé.')
+        return redirect('product_list')
+    return render(request, 'products/product_confirm_delete.html', {'product' : product})
